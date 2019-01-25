a8a5803919858cd47329912003298de92bb1622a
Sergey Taranov <sergey73@yandex.ru>
2019-01-25 21:52:07 +0300

    Fix url to bootstrap

--- dashboard.php
+++ dashboard.php
@@ -60,7 +60,7 @@ $permissions = $user->get_permissions();
     <title>DigiKey authentication</title>
 
     <!-- Bootstrap core CSS -->
-    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="css/dashboard.css">
 
     <!-- Custom styles for this template -->
@@ -167,5 +167,7 @@ $permissions = $user->get_permissions();
     ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
     <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
   </body>
 </html>
